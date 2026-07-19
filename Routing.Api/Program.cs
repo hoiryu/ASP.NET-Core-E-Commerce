e564f882350521535ac0be8801d97cb261665c27
hoiryu <comzkow@gmail.com>
2026-07-19 16:04:39 +0900

    Create Route Constraint

--- Routing.Api/Program.cs
+++ Routing.Api/Program.cs
@@ -1,10 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/products/{id=1}", async (HttpContext context, int id) => Results.Ok(new { message = $"ProductId: {id}" }));
+
 app.MapGet(
-	"/files/{filename=default}.{extension=default}",
-	async (HttpContext context, string filename, string extension) =>
-		Results.Ok(new { message = $"file: {filename}.{extension}" })
+	"/details/{id?}",
+	async (HttpContext context, int? id) =>
+		id is null ? Results.Ok(new { message = "No id provided" }) : Results.Ok(new { message = $"DetailId: {id}" })
+);
+
+app.MapGet(
+	"/daily-digest-report/{reportdate:datetime?}",
+	(HttpContext context, DateTime? reportdate) =>
+		reportdate is null
+			? Results.Ok(new { message = $"Now: {DateTime.Now}" })
+			: Results.Ok(new { message = $"reportdate: {reportdate}" })
 );
 
 app.MapFallback(
